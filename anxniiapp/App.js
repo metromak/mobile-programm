dc6d84fa59005bd79143f02b1dcd8b7786f1d5e5
metromak <stevenadams1025@gmail.com>
2023-02-17 15:03:13 +0800

    1

--- anxniiapp/App.js
+++ anxniiapp/App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import React from 'react'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView horizontal={true} style={styles.scroll}>
+        <View style={styles.draft1}></View>
+        <View style={styles.draft2}></View>
+        <View style={styles.draft3}></View>
+      </ScrollView>
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'brown',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row'
+
   },
-});
+  draft1: {
+    height: 100,
+    width: 100,
+    backgroundColor: 'pink',
+    margin: 10,
+    
+  },
+  draft2: {
+    height: 100,
+    width: 100,
+    backgroundColor: 'aqua',
+    margin: 10,
+    
+  },
+  draft3: {
+    height: 100,
+    width: 100,
+    backgroundColor: 'green',
+    margin: 10,
+    
+  }
+})
